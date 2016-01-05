c352540f49c5e42921abf0e0e7471bf72cef3571
zhang.hai <zhang.hai@founder.com.cn>
2016-01-05 17:00:47 +0800

    还原上次的提交
    Signed-off-by: zhang.hai <zhang.hai@founder.com.cn>

--- jwzh-rule/src/main/java/com/founder/drools/core/model/RuleFileUtil.java
+++ jwzh-rule/src/main/java/com/founder/drools/core/model/RuleFileUtil.java
@@ -150,7 +150,7 @@ public class RuleFileUtil {
 	        		 }
 	        		 
 	        		 if(line.contains("/*BZ:")){//备注
-	        			 rule_head.setBz(line.substring(line.indexOf("/*BZ:")+5,line.indexOf(":BZ*/")));
+	        			 rule_head.setBz(line.substring(line.indexOf("/*BZ:")+5,line.indexOf("*/")));
 	        			 continue;
 	        		 }
 	        		 
@@ -291,7 +291,7 @@ public class RuleFileUtil {
 	         while ((line = br.readLine()) != null) {
 	        	 line=new String(line.getBytes(),"UTF-8");
 	        	 if(line.contains("/*BZ:")){
-	        		 return line.substring(line.indexOf("/*BZ:")+5,line.indexOf(":BZ*/"));
+	        		 return line.substring(line.indexOf("/*BZ:")+5,line.indexOf("*/"));
 	        	 }
 	         }
 		 }catch (Exception e) {
